949f1d07bb2f1cd2546497a634ab0a0c7173e4c8
qq905713813@163.com <qq905713813@163.com>
2020-02-08 16:07:53 +0800

    热重启

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 
 	"ggin/routers"
 	"ggin/pkg/setting"
+	"log"
+	"os"
+	"os/signal"
+	"context"
+	"time"
 )
 
 func main() {
@@ -19,6 +24,23 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)	// 对 Interrupt 信号进行捕捉至 quit 通道
+	<- quit		// 阻塞，通道等待
+
+	log.Println("Shutdown Server ...")
+	ctx, cannel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cannel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 
 }
