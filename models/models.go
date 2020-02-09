7147dee0e318147e902c9d3bd87d36ad3c7c6925
qq905713813@163.com <qq905713813@163.com>
2020-02-09 15:51:26 +0800

    增加删除钩子，实现软连接删除功能

--- models/models.go
+++ models/models.go
@@ -16,7 +16,7 @@ type Model struct {
 	ID         int `gorm:"primary_key" json:"id"`
 	CreatedOn  int `json:"created_on"`
 	ModifiedOn int `json:"modified_on"`
-	//DeleteOn   int `json:"deleted_on"`
+	DeletedOn  int `json:"deleted_on"`
 }
 
 // updateTimeStampForCreateCallback will set `CreatedOn`, `ModifiedOn` when creating
@@ -42,6 +42,41 @@ func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
 	if _, ok := scope.Get("gorm:update_column"); !ok {
 		scope.SetColumn("ModifiedOn", time.Now().Unix())
 	}
+}
+
+func deleteCallBack(scope *gorm.Scope) {
+	if !scope.HasError() {
+		var extraOption string
+		if str, ok := scope.Get("gorm:delete_option"); ok { // 是否手动指定了 delete_option
+			extraOption = fmt.Sprint(str)
+		}
+		deletedOnField, hasDeletedOnField := scope.FieldByName("DeletedOn") // 获取 DeletedOn 字段
+
+		if !scope.Search.Unscoped && hasDeletedOnField { // hasDeletedOnField， 存在 DeletedOn 字段，软删除
+			scope.Raw(fmt.Sprintf(
+				"UPDATE %v SET %v=%v%v%v",
+				scope.QuotedTableName(), // 引用的表名
+				scope.Quote(deletedOnField.DBName),
+				scope.AddToVars(time.Now().Unix()),			// 添加值作为 SQL 参数，预防 SQL 注入
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),		// 组合好的 sql
+				addExtraSpaceIfExist(extraOption),
+			)).Exec()
+		} else { // 不存在 DeletedOn 字段，硬删除
+			scope.Raw(fmt.Sprintf(
+				"DELETE FROM %v%v%v",
+				scope.QuotedTableName(),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption),
+			)).Exec()
+		}
+	}
+}
+
+func addExtraSpaceIfExist(str string) string {
+	if str != "" {
+		return " " + str
+	}
+	return ""
 }
 
 func init() {
@@ -75,7 +110,7 @@ func init() {
 
 	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
 	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
-
+	db.Callback().Delete().Replace("gorm:delete", deleteCallBack)
 
 	db.SingularTable(true)
 	db.DB().SetMaxIdleConns(10) // IdleTimeout：等待的最大时间
---
+++ sqls/addDeleteOn.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `blog_tag` ADD `deleted_on` int(10) unsigned;
+ALTER TABLE `blog_article` ADD `deleted_on` int(10) unsigned;
