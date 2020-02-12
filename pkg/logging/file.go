fcc87f0bc61f7ef1febe972c5888ec37d223da7c
qq905713813@163.com <qq905713813@163.com>
2020-02-12 11:12:57 +0800

    重构：日志器创建

--- pkg/logging/file.go
+++ pkg/logging/file.go
@@ -4,50 +4,43 @@ import (
 	"time"
 	"os"
 	"fmt"
-	"log"
-)
-
-var (
-	LogSavePath = "runtime/logs/"
-	LogSaveName = "log"
-	LogFileExt  = "log"
-	TimeFormat  = "200601012"
+	"ggin/pkg/setting"
+	"ggin/pkg/file"
 )
 
 func getLogFilePath() string {
-	return fmt.Sprintf("%s", LogSavePath)
+	//return fmt.Sprintf("%s", LogSavePath)
+	return fmt.Sprintf("%s%s", setting.AppSetting.RuntimeRootPath, setting.AppSetting.LogSavePath)
 }
 
-func getLogFileFullPath() string {
-	prefixPath := getLogFilePath()
-	suffixPath := fmt.Sprintf("%s%s.%s", LogSaveName, time.Now().Format(TimeFormat), LogFileExt)
-
-	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+func getLogFileName() string {
+	return fmt.Sprintf("%s%s.%s",
+		setting.AppSetting.LogSaveName,
+		time.Now().Format(setting.AppSetting.TimeFormat),
+		setting.AppSetting.LogFileExt,
+	)
 }
 
-func mkDir(filePath string) {
-	dir, _ := os.Getwd()
-	//fmt.Println(filePath)		// runtime/logs/
-	//fmt.Println(getLogFilePath())		// runtime/logs/
-	//err := os.MkdirAll(dir + "/" + getLogFilePath(), os.ModePerm)
-	err := os.MkdirAll(dir + "/" + filePath, os.ModePerm)
+func openLogFile(fileName, filePath string) (*os.File, error) {
+	dir, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("os.Getwd err: %v", err)
 	}
-}
+	src := dir + "/" + filePath
+	perm := file.CheckPermission(src)
+	if perm == true {
+		return nil, fmt.Errorf("file.CheckPermission Permission denied src: %s", src)
+	}
 
-func openLogFile(filePath string) *os.File {
-	_, err := os.Stat(filePath)
-	switch {
-	case os.IsNotExist(err):
-		//fmt.Println(filePath)	// runtime/logs/log202002028.log
-		mkDir(getLogFilePath())
-	case os.IsPermission(err):
-		log.Fatalf("Permission: %v", err)
-	}
-	handle, err := os.OpenFile(filePath, os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+	err = file.IsNotExistMkDir(src)
 	if err != nil {
-		log.Fatalf("Fail to OpenFile : %v", err)
+		return nil, fmt.Errorf("file.IsNotExistMkDir src: %s, err: %v", src, err)
 	}
-	return handle
+
+	f, err := file.Open(src+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("Fail to OpenFile: %v", err)
+	}
+	return f, nil
+
 }
